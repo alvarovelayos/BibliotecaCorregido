3c80a61a45e7bf1deec7283cd8b611371c73f46e
alvarovelayos <alvarovelayos31@gmail.com>
2022-11-13 21:53:34 +0100

    subiendo clase usuario

---
+++ src/User.java
@@ -0,0 +1,56 @@
+public class User {
+    private String dni;
+    private String name;
+    private String surname;
+    private String phone;
+    private String address;
+    private String city;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
