f713be48786ce2ca33a54f62588d4f15b2580af4
alvarovelayos <alvarovelayos31@gmail.com>
2022-11-13 21:51:18 +0100

    subiendo clase autor

---
+++ src/Author.java
@@ -0,0 +1,38 @@
+public class Author {
+    private String name;
+    private String surname;
+    private String dateBorn;
+    private String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDateBorn() {
+        return dateBorn;
+    }
+
+    public void setDateBorn(String dateBorn) {
+        this.dateBorn = dateBorn;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
