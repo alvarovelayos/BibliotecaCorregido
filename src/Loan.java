f6dc229f1a6210b8b13a25120fc97dce3c0a2b45
alvarovelayos <alvarovelayos31@gmail.com>
2022-11-13 22:17:18 +0100

    subiendo clase prestamo

---
+++ src/Loan.java
@@ -0,0 +1,47 @@
+public class Loan {
+    private String id;
+    private String dateStart;
+    private String dateEnd;
+    private User user;
+    private Book book;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(String dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public String getDateEnd() {
+        return dateEnd;
+    }
+
+    public void setDateEnd(String dateEnd) {
+        this.dateEnd = dateEnd;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+}
